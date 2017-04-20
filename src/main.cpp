c5a3056c2f010d3577a12576d6553d2950d0c40d
Julien CAPELLARI <julien@capellari.net>
2017-04-20 09:47:36 +0200

    Corrections Source Tree

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,7 @@
 
 // Main
 int main() {
-<<<<<<< HEAD
-
-    Tableau t= Tableau();
-    t.CreationTab();
-    t.BoucleJeu();
-
-=======
     Tableau t = Tableau();
->>>>>>> 5b9a5e5b88b29d9e28eac04e1f1755033277b66e
 
     std::cout << std::boolalpha;
 
