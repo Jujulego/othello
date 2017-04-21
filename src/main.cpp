f8dc905fe1cfdb27fabf3695f202ac4484bf5ede
DESKTOP-58V0NKN\maxim <maxime.toquebiau@hotmail.fr>
2017-04-21 18:57:53 +0200

    push bug

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,12 @@
 // Main
 int main() {
     // Init MemIAs
-    std::shared_ptr<IA> memia_noire   = std::make_shared<MemIA>("arbre_noir.txt",  PROF_ALGO, NOIR);
+    std::shared_ptr<IA> memia_noire   = std::make_shared<MinMaxIA>(PROF_ALGO, NOIR);
     std::shared_ptr<IA> memia_blanche = std::make_shared<MemIA>("arbre_blanc.txt", PROF_ALGO, BLANC);
-    
+
     // Création du plateau de jeu
 //    Tableau t = Tableau(nullptr, std::make_shared<MinMaxIA>(PROF_ALGO, BLANC));
-    Tableau t = Tableau(memia_noire, memia_blanche);
+    Tableau t = Tableau(memia_noire, nullptr);
     t.BoucleJeu();
 
 	return 1; // Erreur => y a pas de code !!!
