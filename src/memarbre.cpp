a93e55c01ba64aa3df20a37303a59344655c32cf
Julien CAPELLARI <julien@capellari.net>
2017-04-21 18:00:03 +0200

    correction bug tellg et Windows

--- src/memarbre.cpp
+++ src/memarbre.cpp
@@ -19,14 +19,14 @@ bool getline(std::fstream& f, std::string& buf) {
 	// Gestion des erreurs
 	auto mask = f.exceptions();
 	f.exceptions(std::ios::goodbit);
-	
+
 	// Lecture
 	bool ok = bool(std::getline(f, buf));
-	
+
 	// Remise du mask original
 	f.clear();
 	f.exceptions(mask);
-	
+
 	return ok;
 }
 
@@ -59,26 +59,26 @@ std::shared_ptr<MemArbre::Noeud> MemArbre::Noeud::add_fils(int val, std::tuple<i
 	// Création du noeud
 	auto n = std::make_shared<MemArbre::Noeud>(val, coords, -1, arbre(), shared_from_this());
 	m_fils.insert(n);
-	
+
 	// Sauvegarde dans l'arbre
 	arbre()->save(n);
-	
+
 	return n;
 }
 
 void MemArbre::Noeud::save() {
 	// Génération de la nouvelle ligne
 	std::ostringstream data(std::ostringstream::ate);
-	
+
 	// L'adresse du pere
 	data << (m_pere.expired() ? -1 : pere()->pos());
-	
+
 	// Les coordonnees
 	data << " " << (char) (std::get<0>(m_coords) + 'A') << (std::get<1>(m_coords) + 1) << " ";
-	
+
 	// La valeur
     data << m_val;
-	
+
 	// Envoi vers le fichier !
 	arbre()->save(m_pos, data.str());
 }
@@ -131,7 +131,7 @@ std::set<std::shared_ptr<MemArbre::Noeud>> const& MemArbre::Noeud::fils() const 
 // Constructeur
 MemArbre::MemArbre(std::string const& fichier) {
 	// Ouverture du fichier !
-    m_fichier.open(fichier);
+    m_fichier.open(fichier, std::ios_base::in | std::ios_base::out | std::ios_base::binary);
 
     while (m_fichier.fail()) {
         // Uniquement si le fichier n'existe pas
@@ -163,16 +163,16 @@ void MemArbre::charger() {
 	// Lecture !
 	int pos = m_fichier.tellg(), pere = -1, val = 0;
 	std::string coords, buf;
-	
+
 	while (getline(m_fichier, buf)) {
 		// Analyse !
 		std::istringstream s(buf);
-		
+
 		// Extraction des valeurs
 		s >> pere;
 		s >> coords;
 		s >> val;
-		
+
 		// Création du noeud
 		std::shared_ptr<MemArbre::Noeud> n;
 		if (pere == -1) {
@@ -192,9 +192,9 @@ void MemArbre::charger() {
             );
             m_noeuds.at(pere)->add_fils(n);
 		}
-		
+
 		m_noeuds[pos] = n;
-		
+
 		// Position du suivant !
 		pos = m_fichier.tellg();
 	}
@@ -204,7 +204,7 @@ void MemArbre::save(int& pos, std::string const& data) {
 	// Déclarations
 	std::string buf;
 	std::vector<std::string> lignes(1, data);
-	
+
 	// pos == -1 => pas de ligne à changer mais une à rajouter
 	if (pos == -1) {
 		m_fichier.seekg(0, m_fichier.end);
@@ -213,13 +213,13 @@ void MemArbre::save(int& pos, std::string const& data) {
 		// Lecture de la suite du fichier : après la ligne à modifier
 		m_fichier.seekg(pos, m_fichier.beg);
 		getline(m_fichier, buf);
-		
+
 		while (getline(m_fichier, buf)) lignes.push_back(buf);
-		
+
 		// Retour à la ligne à changer
 		m_fichier.seekg(pos, m_fichier.beg);
 	}
-	
+
 	// Modification !
 	for (auto l : lignes) m_fichier << l << std::endl;
 }
@@ -233,22 +233,22 @@ void MemArbre::save(std::shared_ptr<MemArbre::Noeud> const& n) {
 std::shared_ptr<MemArbre::Noeud> MemArbre::add_racine(int val, std::tuple<int,int> const& coords) {
 	// Création du noeud
     auto n = std::make_shared<MemArbre::Noeud>(val, coords, -1, shared_from_this(), nullptr);
-	
+
 	// Sauvegarde dans l'arbre
     save(n);
-	
+
 	return n;
 }
 
 std::set<std::shared_ptr<MemArbre::Noeud>> MemArbre::racines() const {
 	// Déclarations
 	std::set<std::shared_ptr<MemArbre::Noeud>> racines;
-	
+
 	// Parcours de la liste
 	for (auto p : *this) {
 		if (p.second->est_racine()) racines.insert(p.second);
 	}
-	
+
 	return racines;
 }
 
